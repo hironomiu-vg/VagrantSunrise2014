f6b400e27cb8720652dafb5ce4785cef899eaa8e
hironomiu <hironomiu@example.com>
2014-11-04 17:30:08 +0900

    commit

--- puppet/modules/varnish/manifests/config.pp
+++ puppet/modules/varnish/manifests/config.pp
@@ -2,7 +2,6 @@ class varnish::config {
     file { "/etc/sysconfig/varnish":
         owner => "root", group => "root",
         content => template('varnish/varnish'),
-        mode => 600,
         notify => Service['varnish'],
     }
 }
