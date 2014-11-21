9c8361faab3cee4a71bd28e2e9e9e3a15c106ee7
hironomiu <hironomiu@example.com>
2014-11-21 14:49:24 +0900

    commit

--- puppet/modules/php/manifests/install.pp
+++ puppet/modules/php/manifests/install.pp
@@ -1,10 +1,29 @@
 class php::install{
+     yumrepo { 'hhvm-repo':
+        descr => 'hhvm repo',
+        baseurl  => 'http://www.hop5.in/yum/el6/',
+        gpgkey   => 'http://dl.hhvm.com/conf/hhvm.gpg.key',
+        enabled  => 1,
+        gpgcheck => 0,
+        priority => 1,
+    }
+
+    package{
+        [
+        'hhvm',
+        ]:
+        provider => 'yum',
+        ensure => installed,
+        require => Yumrepo['hhvm-repo'],
+    }
+
     yumrepo { 'php-remi':
         descr => 'remi repo',
         mirrorlist => 'http://rpms.famillecollet.com/enterprise/6/remi/mirror',
         enabled    => 1,
         gpgcheck   => 1,
         gpgkey     => 'http://rpms.famillecollet.com/RPM-GPG-KEY-remi',
+        require => Package['hhvm'],
     }
 
     package{
@@ -46,4 +65,6 @@ class php::install{
         ensure => installed,
         require => Yumrepo['php-epel'],
     }
+
+
 }
